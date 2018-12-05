3aaa7b4bf0416cf7ab0b15fdc78def33f3bb72b3
= <=>
2018-12-05 15:36:09 -0200

    not implement yet

--- FastMovies/js/appendCarousel.js
+++ FastMovies/js/appendCarousel.js
@@ -1,52 +1,102 @@
 const filmes = [{
         id: 1,
         titulo: 'O Grinch',
-        posterUrl: 'moviePosters/grinch.jpg'
+        posterUrl: 'moviePosters/grinch.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 2,
         titulo: 'O Quebra-Nozes e os Quatro Reinos',
-        posterUrl: 'moviePosters/quebraNozes.jpg'
+        posterUrl: 'moviePosters/quebraNozes.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 3,
         titulo: 'Pé Pequeno',
-        posterUrl: 'moviePosters/PePequeno.jpg'
+        posterUrl: 'moviePosters/PePequeno.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 4,
         titulo: 'Bohemian Rhapsody',
-        posterUrl: 'moviePosters/d58677de7d7f64d820bfa646a78d95df.jpg'
+        posterUrl: 'moviePosters/d58677de7d7f64d820bfa646a78d95df.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 5,
         titulo: 'Halloween',
-        posterUrl: 'moviePosters/Halloween-2018-Poster-the-halloween-movies-41553252-1294-2048.png'
+        posterUrl: 'moviePosters/Halloween-2018-Poster-the-halloween-movies-41553252-1294-2048.png',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 6,
         titulo: 'Jhonny English 3',
-        posterUrl: 'moviePosters/MV5BMjI4Mj.jpg'
+        posterUrl: 'moviePosters/MV5BMjI4Mj.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 7,
         titulo: 'Venom',
-        posterUrl: 'moviePosters/venompostertransform.jpg'
+        posterUrl: 'moviePosters/venompostertransform.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 8,
         titulo: 'Tudo por um Popstar',
-        posterUrl: 'moviePosters/5582578.jpg'
+        posterUrl: 'moviePosters/5582578.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 9,
         titulo: 'O Doutrinador',
-        posterUrl: 'moviePosters/MV5BMGU4YjQyMGQtM.jpg'
+        posterUrl: 'moviePosters/MV5BMGU4YjQyMGQtM.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     },
     {
         id: 10,
         titulo: 'Assim Nasce uma Estrela',
-        posterUrl: 'moviePosters/a-star-is-born-portuguese-movie-poster.jpg'
+        posterUrl: 'moviePosters/a-star-is-born-portuguese-movie-poster.jpg',
+        background: 'moviePosters/0bdad8a01055d795d2cf0e275cf41751.png',
+        genero: 'Animação | Comédia',
+        sinopse: 'O Grinch é um ser verde que não suporta o Natal e, todo ano, precisa aturar que os habitantes da cidade vizinha de Quemlândia comemoram a data. Decidido a acabar com a festa, ele resolve invadir os lares dos vizinhos e roubar tudo o que está relacionado ao Natal.',
+        imdb_score: '5.0',
+        publico: 'L'
     }
 ]
 
@@ -71,5 +121,179 @@ window.addEventListener('load', () => {
         `
         div.innerHTML = html
         document.querySelector('.resCarousel-inner').appendChild(div)
+
+        // append collappses
+        //appendCollapse(filme)
     })
 })
+
+// function appendCollapse(filme) {
+//     let div = document.createElement('div')
+//     div.className = "collapse multi-collapse"
+//     div.id = `collapseExample${filme.id}`
+//     let html = `
+//     <div class="card card-body movie_poster" style="background: url('${filme.background}') top no-repeat; background-size: cover; object-fit: contain;">
+//     <div class="itemSinopse col-md-6">
+//         <p id="title">${filme.titulo}</p>
+//         <p id="info">${filme.genero}</p>
+//         <p id="info_title">Sinopse</p>
+//         <p id="info_sinop">${filme.sinopse}</p>
+//         <img class="icon yt_icon" src="css/yt_icon.png">
+//         <a class="trailer" href="#">Trailer</a>
+//         <div class="circle imdb_score">${filme.imdb_score}</div>
+//         <div class="circle PG livre">${filme.publico}</div>
+//         <img class="icon imdb_icon" src="css/imdb.png">
+//     </div>
+//     <div class="itemSessoes col-md-6">
+//         <ul class="nav nav-tabs" style="border: none;">
+//             <li class="active"><a data-toggle="tab" href="#home">
+//                     <p class="hour">Dom</p>
+//                     <p class="hour">25/11</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu1">
+//                     <p class="hour">Seg</p>
+//                     <p class="hour">26/11</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu2">
+//                     <p class="hour">Ter</p>
+//                     <p class="hour">27/11</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu3">
+//                     <p class="hour">Qua</p>
+//                     <p class="hour">28/11</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu4">
+//                     <p class="hour">Qui</p>
+//                     <p class="hour">29/11</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu5">
+//                     <p class="hour">Sex</p>
+//                     <p class="hour">30/11</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu6">
+//                     <p class="hour">Sab</p>
+//                     <p class="hour">01/12</p>
+//                 </a></li>
+//             <li><a data-toggle="tab" href="#menu7">
+//                     <p class="hour">Dom</p>
+//                     <p class="hour">02/12</p>
+//                 </a></li>
+//         </ul>
+
+//         <div class="tab-content horarios">
+//             <div id="home1" class="tab-pane fade in active">
+//                 <h3>
+//                     <font color="#ffffff">Domingo</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span>&emsp;18:40 &emsp; 21:10</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span>&emsp; 19:50 &emsp; 22:00</font>
+//                 </p>
+//             </div>
+//             <div id="menu1" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Segunda</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span>&emsp;18:40 &emsp; 21:10</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span>&emsp; 19:50 &emsp; 22:00</font>
+//                 </p>
+//             </div>
+//             <div id="menu2" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Terça</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span>&emsp;18:40 &emsp; 21:10</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span> &emsp; 19:50&emsp; 22:00</font>
+//                 </p>
+//             </div>
+//             <div id="menu3" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Quarta</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span> &emsp; 13:40 &emsp;16:10 &emsp;
+//                         18:40</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span> &emsp; 21:50 &emsp; 22:30</font>
+//                 </p>
+//             </div>
+//             <div id="menu4" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Quinta</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span> &emsp; 13:40 &emsp; 16:10
+//                         &emsp; 18:40</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span> &emsp; 21:50 &emsp; 22:30</font>
+//                 </p>
+//             </div>
+//             <div id="menu5" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Sexta</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span>&emsp;13:40 &emsp; 16:10 &emsp;
+//                         18:40</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span> &emsp;21:50 &emsp; 22:30</font>
+//                 </p>
+//             </div>
+//             <div id="menu6" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Sábado</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span> &emsp; 13:40 &emsp; 16:10
+//                         &emsp; 18:40</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span> &emsp; 21:50 &emsp; 22:30</font>
+//                 </p>
+//             </div>
+//             <div id="menu7" class="tab-pane fade">
+//                 <h3>
+//                     <font color="#ffffff">Sábado</font>
+//                 </h3>
+//                 <p style="margin-top: 15px;"><span class="label dublado">Dublado</span></p>
+//                 <p style="margin-top:15px;">
+//                     <font color="#ffffff"><span class="label f3D">3D</span> &emsp; 13:40 &emsp; 16:10
+//                         &emsp; 18:40</font>
+//                 </p><br>
+//                 <p style="margin-top:15px;"><span class="label legendado">Legendado</span></p>
+//                 <p style="margin-top: 15px;">
+//                     <font color="#ffffff"><span class="label f2D">2D</span> &emsp; 21:50 &emsp; 22:30</font>
+//                 </p>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+//     `
+//     div.innerHTML = html
+//     document.querySelector('#divContainer').appendChild(div)
+// }
